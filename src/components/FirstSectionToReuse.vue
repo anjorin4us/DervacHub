8120f24c0a88d0fece49a013a21af5bff213e9a5
unknown <you@example.com>
2024-10-11 22:08:50 +0100

    building

--- src/components/FirstSectionToReuse.vue
+++ src/components/FirstSectionToReuse.vue
@@ -15,8 +15,8 @@ export default defineComponent({
     <div class="relative w-screen py-40 md:px-32 px-4 text-center bg-gradient-to-l from-[#f2f9ff] via-[#f2f7fd] to-[#f3dee3]">
         <p class="md:text-[50px] text-[30px] font-bold z-30">{{ heading }}</p>
         <p class="md:text-[20px] text-[15px] text-[#475467] z-30 mt-2">{{ caption }}</p>
-        <img src="others/blue-curve-to-re-use.svg" alt="" class="absolute right-0 top-0">
-        <img src="others/red-curve-to-re-use.svg" alt="" class="absolute left-0 bottom-0">
+        <img src="/others/blue-curve-to-re-use.svg" alt="" class="absolute right-0 top-0">
+        <img src="/others/red-curve-to-re-use.svg" alt="" class="absolute left-0 bottom-0">
     </div>
 </template>
 
